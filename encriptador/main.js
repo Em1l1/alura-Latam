803adeb5480df6edf1bf4cd9502eb820931de954
Emily <75467880+Em1l1@users.noreply.github.com>
2022-09-29 15:18:38 -0600

    logical encriptador

---
+++ encriptador/main.js
@@ -0,0 +1,25 @@
+const inputTexto = document.querySelector(".input-texto");
+const mensaje = document.querySelector(".mensaje");
+
+/*
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"
+
+*/
+
+function encriptar(stringEncriptada){
+  let matrizCodigo = [
+    ["e","enter"],
+    ["i","imes"],
+    ["a","ai"],
+    ["o","ober"],
+    ["u","ufat"]
+  ];
+  stringEncriptada = stringEncriptada.toLowerCase();
+
+  for(let i = 0; i  matrizCodigo.length;)
+
+}
