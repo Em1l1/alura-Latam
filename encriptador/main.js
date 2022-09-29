1731dabaac054f88b00a5a680a67470e4fc6f855
Emily <75467880+Em1l1@users.noreply.github.com>
2022-09-29 16:01:30 -0600

    create function
    function encriptar,
    function desencriptar,
    function copiar.

--- encriptador/main.js
+++ encriptador/main.js
@@ -1,6 +1,14 @@
 const inputTexto = document.querySelector(".input-texto");
 const mensaje = document.querySelector(".mensaje");
 
+function btnEncriptar(){
+  const TextoEncriptado = encriptar(inputTexto.value);
+  mensaje.value = TextoEncriptado;
+  mensaje.style.backgroundImage = "none";
+  inputTexto.value = ""
+}
+
+
 /*
 La letra "e" es convertida para "enter"
 La letra "i" es convertida para "imes"
@@ -20,6 +28,46 @@ function encriptar(stringEncriptada){
   ];
   stringEncriptada = stringEncriptada.toLowerCase();
 
-  for(let i = 0; i  matrizCodigo.length;)
+  for(let i = 0; i < matrizCodigo.length; i++){
+    if(stringEncriptada.includes(matrizCodigo[i][0])){
+      stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+    }
+  }
 
+  return stringEncriptada;
+}
+
+// desencriptar
+function btnDesencriptar(){
+  const TextoEncriptado = desencriptar(inputTexto.value);
+  mensaje.value = TextoEncriptado;
+  inputTexto.value = ""
+}
+
+function desencriptar(stringDesencriptada){
+  
+  let matrizCodigo = [
+    ["e","enter"],
+    ["i","imes"],
+    ["a","ai"],
+    ["o","ober"],
+    ["u","ufat"]
+  ];
+  stringDesencriptada = stringDesencriptada.toLowerCase();
+
+  for(let i = 0; i < matrizCodigo.length; i++){
+    if(stringDesencriptada.includes(matrizCodigo[i][1])){
+      stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+    }
+  }
+
+  return stringDesencriptada;
+}
+
+function copiar(){
+  mensaje.select()
+
+  navigator.clipboard.writeText(mensaje.value)
+  mensaje.value = ""
+  alert("Texto copiado")
 }
