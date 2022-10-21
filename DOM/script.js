00418f79103e8d1b9a9a66d56eccd06918864b1e
Victoriano <mytacam78@gmail.com>
2022-10-21 03:08:47 -0500

    Add class Toggle ⛽

--- DOM/script.js
+++ DOM/script.js
@@ -8,13 +8,20 @@ const createTask = (evento) => {
   const task = document.createElement("li");
   task.classList.add("card");
   input.value = "";
+
   //backtricks
-  const content = `<div>
-      <i class="far fa-check-square icon"></i>
-      <span class="task">${value}</span>
-  </div>
+  const taskContent = document.createElement("div");
+
+  const titleTask = document.createElement("span");
+  titleTask.classList.add("task");
+  titleTask.innerHTML = value;
+  taskContent.appendChild(checkComplete());
+  taskContent.appendChild(titleTask)
+  
+  const content = `
   <i class="fas fa-trash-alt trashIcon icon"></i>`;
-  task.innerHTML = content;
+  
+  task.appendChild(taskContent);
   list.appendChild(task);
   console.log(content);
 };
@@ -24,3 +31,19 @@ console.log(btn);
 // listener
 // Arrow functions o funciones anonimas
 btn.addEventListener("click", createTask);
+
+const checkComplete = () => {
+  const i = document.createElement("i");
+  i.classList.add("far", "fa-check-square", "icon");
+  i.addEventListener("click", completeTask)
+  return i;
+};
+
+const completeTask = (event) => {
+  // console.log("Complete Task")
+  const element = event.target
+  element.classList.toggle("fas")
+  element.classList.toggle("completeIcon")
+  element.classList.toggle("far")
+  
+}
