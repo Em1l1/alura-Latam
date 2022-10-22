6aaa309e2b4054f577a8d93016c3c34bb8e1991e
Victoriano <mytacam78@gmail.com>
2022-10-22 02:01:15 -0500

    fetch API | https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API | ⥤ Co-authored-by @Em1l1 <blancocronos@hotmail.com>

--- crud-Js/service/client-service.js
+++ crud-Js/service/client-service.js
@@ -35,24 +35,9 @@ const table = document.querySelector("[data-table]");
 // update - PUT/PATCH
 // delete - DELETE
 
-const listaClientes = () => {
-  const promise = new Promise((resolve, reject) => {
-    const http = new XMLHttpRequest();
-    http.open("GET", "http://localhost:3000/perfil");
-
-    http.send();
-
-    http.onload = () => {
-      const response = JSON.parse(http.response);
-      if (http.status >= 400) {
-        reject(response);
-      } else {
-        resolve(response);
-      }
-    };
-  });
-  return promise;
-};
+// Fetch API
+const listaClientes = () =>
+  -fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
 
 listaClientes()
   .then((data) => {
@@ -61,5 +46,4 @@ listaClientes()
       table.appendChild(nuevaLinea);
     });
   })
-  .catch((error) => alert("Error"))
-
+  .catch((error) => alert("Error"));
