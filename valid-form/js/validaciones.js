82fc6c44039e4976b5cc2f81dc1d80c2072d4462
Victoriano <mytacam78@gmail.com>
2022-11-06 00:38:14 -0600

    add Object describe errror

--- valid-form/js/validaciones.js
+++ valid-form/js/validaciones.js
@@ -3,7 +3,31 @@ export function valida(input) {
   if (validadores[tipoDeInput]) {
     validadores[tipoDeInput](input);
   }
+
+  if (input.validity.valid) {
+    input.parentElement.classList.remove("input-container--invalid");
+  } else {
+    input.parentElement.classList.add("input-container--invalid");
+  }
 }
+
+const mensajeDeError = {
+  nombre: {
+    valueMissing: "Este campo no puede estar vacion",
+  },
+  email: {
+    valueMissing: "Este campo no puede estar vacion",
+    typeMissing: "El correo no es valido",
+  },
+  password: {
+    valueMissing: "Este campo no puede estar vacion",
+    patternMissmatch: "Al menos 6 caracteres, maximo 12, debe contener una letra minuscula, una letra mayuscula, un numero y no puede contener caracteres especiales"
+  },
+  nacimiento: {
+    valueMissing: "Este campo no puede estar vacion",
+    customError: "Debes tener al menos 18 years old"
+  }
+};
 
 const validadores = {
   nacimiento: (input) => validarNacimiento(input),
