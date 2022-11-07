ed1dee216086d786fa78607827e4fbadd405b824
Emily <75467880+Em1l1@users.noreply.github.com>
2022-11-06 21:45:38 -0800

    objects function form

--- valid-form/js/validaciones.js
+++ valid-form/js/validaciones.js
@@ -6,25 +6,34 @@ export function valida(input) {
 
   if (input.validity.valid) {
     input.parentElement.classList.remove("input-container--invalid");
+    input.parentElement.querySelector(".input-container--invalid").innerHTML = ""
   } else {
     input.parentElement.classList.add("input-container--invalid");
+    input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeDeError(tipoDeInput, input)
   }
 }
 
+const tiposDeErrores = [
+  "valueMissing",
+  "typeMismatch",
+  "patternMismatch",
+  "customError"
+]
+
 const mensajeDeError = {
   nombre: {
-    valueMissing: "Este campo no puede estar vacion",
+    valueMissing: "Este campo nombre no puede estar vacion",
   },
   email: {
-    valueMissing: "Este campo no puede estar vacion",
+    valueMissing: "Este campo email no puede estar vacion",
     typeMissing: "El correo no es valido",
   },
   password: {
-    valueMissing: "Este campo no puede estar vacion",
+    valueMissing: "Este campo password no puede estar vacion",
     patternMissmatch: "Al menos 6 caracteres, maximo 12, debe contener una letra minuscula, una letra mayuscula, un numero y no puede contener caracteres especiales"
   },
   nacimiento: {
-    valueMissing: "Este campo no puede estar vacion",
+    valueMissing: "Este campo fecha de nacimiento no puede estar vacion",
     customError: "Debes tener al menos 18 years old"
   }
 };
@@ -32,6 +41,19 @@ const mensajeDeError = {
 const validadores = {
   nacimiento: (input) => validarNacimiento(input),
 };
+
+function mostrarMensajeDeError(tipoDeInput, input) {
+  let mensaje = ""
+  tiposDeErrores.forEach(error => {
+    if (input.validity[error]) {
+      console.log(tipoDeInput, error)
+      console.log(input.validity[error])
+      console.log(mensajeDeError[tipoDeInput][error])
+      mensaje = mensajeDeError[tipoDeInput][error]
+    }
+  })
+  return mensaje
+}
 
 function validarNacimiento(input) {
   const fechaCliente = new Date(input.value);
