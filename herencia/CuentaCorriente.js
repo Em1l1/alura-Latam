5f8d27b7f628fe787e2795facfd295a32ae5ac28
victoriano <mytacam78@gmail.com>
2022-11-11 16:35:59 -0600

    start project

--- herencia/CuentaCorriente.js
+++ herencia/CuentaCorriente.js
@@ -27,24 +27,5 @@ export class CuentaCorriente {
     CuentaCorriente.cantidadCuentas++;
   }
 
-  depositoEnCuenta(valor) {
-    if (valor > 0) 
-      this.#saldo += valor;
-    return this.#saldo;
-  }
 
-  retirarDeCuenta(valor) {
-    if (valor <= this.#saldo) 
-      this.#saldo -= valor;
-    return this.#saldo;
-  }
-
-  verSaldo() {
-    return this.#saldo;
-  }
-
-  transferirParaCuenta(valor, cuentaDestino) {
-    this.retirarDeCuenta(valor);
-    cuentaDestino.depositoEnCuenta(valor);
-  }
 }
--- herencia/index.js
+++ herencia/index.js
@@ -1,6 +1,7 @@
 /* import class */
 import { Cliente } from "./Cliente.js";
 import { CuentaCorriente } from "./CuentaCorriente.js";
+import { CuentaAhorro } from './CuentaAhorro.js';
 
 const cliente = new Cliente("Leonardo", "123456", "123321");
 const cliente2 = new Cliente("Maria", "123142", "89899");
@@ -8,8 +9,6 @@ const cliente2 = new Cliente("Maria", "123142", "89899");
 const cuentaDeLeonardo = new CuentaCorriente(cliente, "1", "001");
 const cuentaDeMaria = new CuentaCorriente(cliente2, "2", "001");
 
-let saldo = cuentaDeLeonardo.verSaldo();
-
-saldo = cuentaDeLeonardo.depositoEnCuenta(800);
-
-console.log(CuentaCorriente.cantidadCuentas);
+const cuentaAhorroLeonardo = new CuentaAhorro(cliente, '9985', '001', 0)
+console.log(cuentaDeLeonardo)
+console.log(cuentaAhorroLeonardo)
